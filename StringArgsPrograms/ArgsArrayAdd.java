934969190e068d5736dbb126c37372fb1d904943
Vaibhav Nagargoje <144002018+mrprocoderr@users.noreply.github.com>
2024-06-10 18:14:38 +0530

    Add files via upload

---
+++ StringArgsPrograms/ArgsArrayAdd.java
@@ -0,0 +1,59 @@
+public class ArgsArrayAdd {
+    public static void main(String[] ar) {
+        // int i1 =Integer.parseInt(args[0]);
+        // int i2 =Integer.parseInt(args[1]);
+        // System.out.println(i1+i2);
+
+        // int for = 4;
+        // System.out.println(for);  //error
+
+        // byte b= 124;
+        // System.out.println(b);    //exicute
+
+
+        // double d=1.2D;
+        // System.out.println(d);  //exicute
+        
+        // boolean b=1;
+        // System.out.println(b);  // int can not converted to boolean
+
+          int a=10,b=017,c=0X3A;
+          System.out.println(a+","+b+","+c);  //output is 10,15,58
+
+        // int 9A=10;
+        // System.out.println(9A); // Not a Statement
+
+        // int x;
+        // System.out.println(x);  //variable x might not have been initialized
+    
+        // int x=10;
+        // int y=5;
+        // System.out.println(++x+(++y)); // Ansure is 17
+
+        // String s1="Hello";
+        // String s2="Vaibhav";
+        // System.out.println(s1.equals(s2));  //false
+
+
+        //Biwise & operator
+        // int x=7;  //0111
+        // int y=9;  //1001
+        // int z= x & y;   //  0001
+        // System.out.println("z = "+z);  // output is 1
+        
+        // int x=7;  //0111
+        // int y=9;  //1001
+        // int z= x | y;   //  1111       // inclusive OR 
+        // System.out.println("z = "+z);  // output is 15
+
+        // int x=7;  //0101
+        // int y=9;  //1001
+        // int z= x ^ y;   //  1100       // exclusive OR
+        // System.out.println("z = "+z);  // output is 14
+
+        // int i_val=10,j_val=20;
+        // boolean chk;
+        // chk=i_val<j_val;
+        // System.out.println(chk); //true
+    }
+}
---
+++ StringArgsPrograms/ArgsArrayPrint.java
@@ -0,0 +1,12 @@
+public class ArgsArrayPrint {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+
+        //  for(int i=0;i<10;i++){
+        //     System.out.println(args[i]);
+        // }
+
+    }
+}
